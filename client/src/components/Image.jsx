b33b0b6dd6e37f93184a739cf0c8d7c17252603d
Vicky H <63320567+vicky0h@users.noreply.github.com>
2023-06-27 15:44:37 +0800

    Update Image.jsx updated image src

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function Image({src, ...rest}) {
-    src = src && src.includes("https://") ? "uploads/" + src : "http://localhost:4000/" + src;
-
+    // src = src && src.includes("https://") ? "uploads/" + src : "http://localhost:4000/" + src;
+    src = src && src.includes("https://") ? "uploads/" + src : "https://dentalclinic.onrender.com/" + src;
   return (
     <img {...rest} src={src} alt={""}/>
   )
